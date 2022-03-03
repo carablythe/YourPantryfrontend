e1471b4721883b39b6fc55a877319f0f75f34320
Cara Phillips <carablythephillips@gmail.com>
2022-03-03 20:04:51 +0900

    trying to get link to work for recipes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   const [newDish, setNewDish] = useState('');
   const [newIngredients, setNewIngredients] = useState('');
   const [newDirections, setNewDirections] = useState('');
+  const [newURL, setNewURL] = useState('');
   const [newPicture, setNewPicture] = useState('');
   const [recipes, setRecipes] = useState([]);
   const [query, setQuery] = useState("")
@@ -37,6 +38,10 @@ const App = () => {
     setNewDirections(event.target.value)
   }
 
+  const handleNewURLChange = (event) => {
+    setNewURL(event.target.value)
+  }
+
   const handleNewPictureChange = (event) => {
     setNewPicture(event.target.value)
   }
@@ -50,6 +55,7 @@ const App = () => {
         dish: newDish,
         ingredients: newIngredients,
         directions: newDirections,
+        recipeURL: newURL,
         picture: newPicture
 
       }
@@ -82,6 +88,7 @@ const App = () => {
               dish: newDish,
               ingredients: newIngredients,
               directions: newDirections,
+              recipeURL: newURL,
               picture: newPicture
             }
           ).then(()=> {
@@ -102,16 +109,18 @@ const App = () => {
         <br/>
         <h4> Search by ingredient(s): <input placeholder="Enter Query" onChange={event => setQuery(event.target.value)} /> </h4>
         <br/>
+        <h2>Got a Recipe to Share? Please Add it Here:</h2>
         <form onSubmit = {handleNewRecipeFormSubmit}>
         Dish/Recipe Name: <input type ="text" onChange={handleNewDishChange}/><br/>
-        Ingredients: <input type ="text" onChange={handleNewIngredientsChange}/><br/>
-        Directions/Preparation Method: <input type ="text" onChange={handleNewDirectionsChange}/><br/>
-        Add a photo of the dish (URL): <input type ="text" onChange={handleNewPictureChange}/><br/>
+        Main Ingredients (separated by commas): <input type ="text" onChange={handleNewIngredientsChange}/><br/>
+        Directions/Preparation Method (optional): <input type ="text" onChange={handleNewDirectionsChange}/><br/>
+        Or instead of directions, add a link(URL) to the recipe: <input type ="text" onChange={handleNewURLChange}/><br/>
+        Add a photo(image URL) of the dish (optional): <input type ="text" onChange={handleNewPictureChange}/><br/>
         <input type="submit" class ="addRecipe" value="Add to List"/>
         </form>
       </section>
       <section>
-      <h2>Browse Recipes Below:</h2>
+      <h2>Browse Recipes "Your Pantry" Collection Below:</h2>
       <ul>
       {recipes.filter(recipe => {
                 if (query === '') {
@@ -124,7 +133,8 @@ const App = () => {
             <h3>{recipe.dish}</h3>
             <h4>Ingredients: {recipe.ingredients}</h4>
             <h4>Directions: {recipe.directions}</h4>
-            <img src= {recipe.picture} alt= "recipePicture"/>
+            <h4>URL:{recipe.recipeURL}</h4>
+            <img src= {recipe.picture}/>
           <h5><em>To edit this recipe's information, enter the new piece of information in the form at the top of the page and then click this button: </em><button class = "editButton" onClick = { (event) => { handleEdit(recipe) } }>Approve Changes to this Recipe's Information</button></h5>
           <button class = "removeButton" onClick = { (event) => { handleDelete(recipe)} }>Remove Recipe Above from Directory</button>
           </li>
